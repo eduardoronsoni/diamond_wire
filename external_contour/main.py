4cdc0cf37a920ebaf4c9fb8d9d773b5dbbca4ce2
eduardoronsoni <eduardoronsoni@outlook.com>
2022-06-23 15:36:06 -0300

    adding crop and scale functions

--- external_contour/main.py
+++ external_contour/main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-import os
 
 
 def rescale(img, pct):
@@ -14,18 +13,37 @@ def rescale(img, pct):
     """
 
     scale_pct = pct
-    w = int(img.shape[1] * scale_pct / 100)
-    h = int(img.shape[0] * scale_pct / 100)
-    dim = (w, h)
+    w = int(img.shape[1] * scale_pct / 100)  # width
+    h = int(img.shape[0] * scale_pct / 100)  # height
+    dim = (w, h)  # new image dimension
 
     imgResized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
     return imgResized
 
 
+def crop(img, x, y):
+    """ 
+    Crop the image 
+    x - maximum x coordinate of pixels (width)
+    y - maximum height you want
+    img - image you want to change the size
+
+    """
+
+    w = int(img.shape[1])  # width
+    h = int(img.shape[0])  # heigth
+
+    img = img[(0:y), (0:x)]
+
+    return img
+
+
 path = '../images/fio_2.1.tif'  # path of images directory
 
 img = cv2.imread(path, 0)  # get grayscale image
 
+img = rescale(img, 50)
 
-print(img.shape)
+
+print(img.shape[0])
