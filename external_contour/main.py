6bfb5d13faffa041c32b1dbc9571d2076abf3cf7
eduardoronsoni <eduardoronsoni@outlook.com>
2022-07-07 15:36:41 -0300

    trying to get nominal diameter

--- external_contour/main.py
+++ external_contour/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from itertools import chain
+from collections import Counter
 
 
 def rescale(img, pct):
@@ -78,12 +79,17 @@ for contour in largest_contours:
     print('shape :', contour.shape)
     print('dimension: ', contour.ndim)
     max = 0
+    upper_min = 0  # local minimum of the "lower" edge for nominal diameter
     min = contour.max()
+    lower_min = contour.max()  # local minimum of the "upper" edge for nominal diameter
 
+
+# looping through every pixel of the contour
     for i in np.ndindex(contour.shape[:2]):
 
-        #print('i: ', i[0])
+        # print('i: ', i[0])  # [0])
         # print('contour i: ', contour[i])  # [1])
+        # print('np index: ', np.ndindex(contour.shape[:2]).max())
 
         if contour[i][1] > max:
             max = contour[i][1]
@@ -93,16 +99,39 @@ for contour in largest_contours:
             min = contour[i][1]
             x_min = contour[i][0]
 
-    diam = max - min
-    edges = np.array([[[x_max, max], [x_min, min]]])
-    cv2.drawContours(img_draw, edges, -1, (0, 0, 255), 2)
-    max_point = cv2.circle(img_point, (x_max, max), radius=5,
-                           color=(255, 0, 0), thickness=-1)
-    min_point = cv2.circle(img_point, (x_min, min), radius=5,
-                           color=(255, 0, 0), thickness=-1)
+    max_diam = max - min
 
-    print('max y: ', max)
-    print('min y: ', min)
+
+for i in np.ndindex(contour.shape[:2]):
+    # delimitating a workspace
+    if ((contour[i][1] >= min) & (contour[i][1] <= (min + abs(max_diam - max)/2)) & (contour[i][0] >= x_max)):
+        if contour[i][1] >= upper_min:
+            upper_min = contour[i][1]
+            x_upper = contour[i][0]
+
+    if ((contour[i][1] <= max) & (contour[i][1] >= (max - abs(max_diam - max)/2)) & (contour[i][0] >= x_min)):
+        if contour[i][1] <= lower_min:
+            lower_min = contour[i][1]
+            x_lower = contour[i][0]
+
+    nominal_diam = abs(lower_min - upper_min)
+
+edges = np.array([[[x_max, max], [x_min, min]]])
+cv2.drawContours(img_draw, edges, -1, (0, 0, 255), 2)
+max_point = cv2.circle(img_point, (x_max, max), radius=5,
+                       color=(255, 0, 0), thickness=-1)
+min_point = cv2.circle(img_point, (x_min, min), radius=5,
+                       color=(255, 0, 0), thickness=-1)
+
+upper_diam = cv2.circle(img_point, (x_upper, upper_min), radius=5,
+                        color=(0, 255, 0), thickness=-1)
+lower_diam = cv2.circle(img_point, (x_lower, lower_min), radius=5,
+                        color=(0, 255, 0), thickness=-1)
+
+print('upper diam: ', upper_min)
+print('lower diam: ', lower_min)
+print('max y: ', max)
+print('min y: ', min)
 
 # Showing Images
 cv2.imshow('Contours Image', img_copy)
