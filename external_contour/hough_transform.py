14257f96d35606ddb483f10ec764e13d4199610c
Eduardo <eduardoronsoni@outlook.com>
2022-09-06 09:21:43 -0300

    Getting max and min diameter points

--- external_contour/hough_transform.py
+++ external_contour/hough_transform.py
@@ -115,64 +115,82 @@ for points in lines:
     # Maintain a simples lookup list for points
     lines_list.append([(x1,y1),(x2,y2)])
 
-print(lines_list)
-#print(len(largest_contours))
+# print(lines_list)
+# #print(len(largest_contours))
 
-print(len(lines_list))
+# print(len(lines_list))
 
-for item in lines_list:
-    for i in item:
-        print(i[1])##getting all y in list of lines from hough transform
+# for item in lines_list:
+#     for a in item:
+#         print(a[1])##getting all y in list of lines from hough transform
 
-# # ----------------------------------------USER MICROSCOPE ZOOM IMPUT------------------------------------------------------------
-# values_list = [50, 250]
-# print(type(values_list))
-# value = input(
-#     f'Insira o valor da escala do microscópio de acordo com a lista a seguir: {values_list} \n ')
-# print(f'Você inseriu o valor {value} ')
-# value = int(value)
+# ----------------------------------------USER MICROSCOPE ZOOM IMPUT------------------------------------------------------------
+values_list = [50, 250]
+print(type(values_list))
+value = input(
+    f'Insira o valor da escala do microscópio de acordo com a lista a seguir: {values_list} \n ')
+print(f'Você inseriu o valor {value} ')
+value = int(value)
 
-# if value in values_list:
-#     print(f'Valor válido ...')
-# else:
-#     print(
-#         f'ERRO: Valor inserido não disponível na lista de valores, código será encerrado')
-#     sys.exit()
+if value in values_list:
+    print(f'Valor válido ...')
+else:
+    print(
+        f'ERRO: Valor inserido não disponível na lista de valores, código será encerrado')
+    sys.exit()
 
-# fct = (amplification(value))/(pct/100)
-# # ------------------------------------------------LOOPING THROUGH LIST (ONLY 1 ARRAY ON THE LIST)-------------------------------------
-# for contour in largest_contours:
+fct = (amplification(value))/(pct/100)
+# ------------------------------------------------LOOPING THROUGH LIST (ONLY 1 ARRAY ON THE LIST)-------------------------------------
+for contour in largest_contours:
 
-#     print('shape :', contour.shape)
-#     print('dimension: ', contour.ndim)
-#     max = 0
+    print('shape :', contour.shape)
+    print('dimension: ', contour.ndim)
+    max = 0
 
-#     min = contour.max()
+    min = contour.max()
 
-#     list_contours = []
+    list_contours = []
 
-# # -------------------------------------------------LOOPING THROUGH EVERY ITEM OF  THE CONTOUR-----------------------------------------
-#     for i in np.ndindex(contour.shape[:2]):
+# -------------------------------------------------LOOPING THROUGH EVERY ITEM OF  THE CONTOUR-----------------------------------------
+    for i in np.ndindex(contour.shape[:2]):
 
-#         # print('i: ', i[0])  # [0])
-#         # print('contour i: ', contour[i])  # [1])
-#         # print('np index: ', np.ndindex(contour.shape[:2]).max())
+        # print('i: ', i[0])  # [0])
+        # print('contour i: ', contour[i])  # [1])
+        # print('np index: ', np.ndindex(contour.shape[:2]).max())
 
-#         if contour[i][1] > max:
-#             max = contour[i][1]
-#             x_max = contour[i][0]
+        if contour[i][1] > max:
+            max = contour[i][1]
+            x_max = contour[i][0]
 
-#         if contour[i][1] < min:
-#             min = contour[i][1]
-#             x_min = contour[i][0]
+        if contour[i][1] < min:
+            min = contour[i][1]
+            x_min = contour[i][0]
 
-#         list_contours.append((contour[i][0], contour[i][1]))
+        list_contours.append((contour[i][0], contour[i][1]))
 
-#     max_diam = abs(max - min)
-#     max_radius = abs(max - min)/2
+    max_diam = abs(max - min)
+    max_radius = abs(max - min)/2
 
-#     upper_min = min  # local minimum of the "lower" edge for nominal diameter
-#     lower_min = contour.max()  # local minimum of the "upper" edge for nominal diameter
+    upper_min = min  # local minimum of the "lower" edge for nominal diameter
+    lower_min = contour.max()  # local minimum of the "upper" edge for nominal diameter
+
+    for item in lines_list:##getting all y in list of lines from hough transform
+        for a in item:
+            if ((a[1] >= min) & (a[1] <= (min + max_radius)) & (a[0] >=10) & (a[0] <= (img.shape[1] - 10))):
+                if a[1] >= upper_min: 
+                    print('veio no primeiro loop')
+                    upper_min = a[1]
+                    x_upper = a[0]
+
+
+            if ((a[1] <= max) & (a[1] >= (max - max_radius)) & (a[0] >= 10) & (a[0] <= (img.shape[1] - 10))):
+                if a[1] <= lower_min:
+                    print('Veio no segundo loop')
+                    lower_min = a[1]
+                    x_lower = a[0]
+    
+    nominal_diam = abs(lower_min - upper_min)
+
 
 #     for i in np.ndindex(contour.shape[:2]):
 #         # delimitating a workspace
@@ -186,10 +204,10 @@ for item in lines_list:
 #                 lower_min = contour[i][1]
 #                 x_lower = contour[i][0]
 
-#     nominal_diam = abs(lower_min - upper_min)
+# nominal_diam = abs(lower_min - upper_min)
 
 
-# # ---------------------------------------LOOPING THROUGH EVERY PIXEL-------------------------------------------------------
+# ---------------------------------------LOOPING THROUGH EVERY PIXEL-------------------------------------------------------
 # list_upper = []
 # list_lower = []
 
@@ -216,47 +234,47 @@ for item in lines_list:
 # average_upper = Average(list_upper)
 # average_lower = Average(list_lower)
 
-# # ---------------------------------------PRINTING RESULTS----------------------------------------------------------------------
-# # print('List of Contours: ', list_contours)
-# # print('(All the measures showed below are in number of pixels)')
-# # print('upper nominal: ', upper_min)
-# # print('lower nominal: ', lower_min)
-# print(f'minimum diameter:{fct*nominal_diam} µm')
+# ---------------------------------------PRINTING RESULTS----------------------------------------------------------------------
+# print('List of Contours: ', list_contours)
+# print('(All the measures showed below are in number of pixels)')
+# print('upper nominal: ', upper_min)
+# print('lower nominal: ', lower_min)
+print(f'minimum diameter:{fct*nominal_diam} µm')
 # print(f'mean diameter: {fct*(round(abs(average_lower - average_upper)))} µm')
-# print(f'maximum diameter:{fct*max_diam} µm')
-# # print('max y: ', max)
-# # print('min y: ', min)
-# # print('mean upper :', round(average_upper))
-# # print('mean lower :', round(average_lower))
+print(f'maximum diameter:{fct*max_diam} µm')
+# print('max y: ', max)
+# print('min y: ', min)
+# print('mean upper :', round(average_upper))
+# print('mean lower :', round(average_lower))
 
 
-# edges = np.array([[[x_max, max], [x_min, min]]])
-# nominal = np.array([[[x_upper, upper_min], [x_lower, lower_min]]])
+edges = np.array([[[x_max, max], [x_min, min]]])
+nominal = np.array([[[x_upper, upper_min], [x_lower, lower_min]]])
 # mean = np.array([[[round((img.shape[1])/2), int(average_upper)],
 #                 [round((img.shape[1])/2), int(average_lower)]]])
 
-# cv2.drawContours(img_draw, edges, -1, (0, 0, 255), 2)
-# cv2.drawContours(img_draw, nominal, -1, (255, 0, 0), 2)
+cv2.drawContours(img_draw, edges, -1, (0, 0, 255), 2)
+cv2.drawContours(img_draw, nominal, -1, (255, 0, 0), 2)
 # cv2.drawContours(img_draw, mean, -1, (0, 255, 0), 2)
 
-# max_point = cv2.circle(img_point, (x_max, max), radius=5,
-#                        color=(255, 0, 0), thickness=-1)
-# min_point = cv2.circle(img_point, (x_min, min), radius=5,
-#                        color=(255, 0, 0), thickness=-1)
-# upper_diam = cv2.circle(img_point, (x_upper, upper_min), radius=5,
-#                         color=(0, 255, 0), thickness=-1)
-# lower_diam = cv2.circle(img_point, (x_lower, lower_min), radius=5,
-#                         color=(0, 255, 0), thickness=-1)
-# # upper_mean_diam = cv2.circle(img_point, (240, int(round(average_upper))), radius=5,
-# #                             color=(0, 0, 255), thickness=-1)
-# # lower_mean_diam = cv2.circle(img_point, (240, int(round(average_lower))), radius=5,
-# #                             color=(0, 0, 255), thickness=-1)
+max_point = cv2.circle(img_point, (x_max, max), radius=5,
+                       color=(255, 0, 0), thickness=-1)
+min_point = cv2.circle(img_point, (x_min, min), radius=5,
+                       color=(255, 0, 0), thickness=-1)
+upper_diam = cv2.circle(img_point, (x_upper, upper_min), radius=5,
+                        color=(0, 255, 0), thickness=-1)
+lower_diam = cv2.circle(img_point, (x_lower, lower_min), radius=5,
+                        color=(0, 255, 0), thickness=-1)
+# upper_mean_diam = cv2.circle(img_point, (240, int(round(average_upper))), radius=5,
+#                             color=(0, 0, 255), thickness=-1)
+# lower_mean_diam = cv2.circle(img_point, (240, int(round(average_lower))), radius=5,
+#                             color=(0, 0, 255), thickness=-1)
 
-# # ---------------------------------------PLOTTING IMAGES---------------------------------------------------------------------
-# cv2.imshow('Contours Image', img_copy)
-# cv2.imshow('Test Image', img_draw)
-# cv2.imshow('Max Point', img_point)
-# cv2.imshow('detectedLines.png',img)
-# cv2.waitKey(0)
+# ---------------------------------------PLOTTING IMAGES---------------------------------------------------------------------
+cv2.imshow('Contours Image', img_copy)
+cv2.imshow('Test Image', img_draw)
+cv2.imshow('Max Point', img_point)
+cv2.imshow('detectedLines.png',img)
+cv2.waitKey(0)
 
-# cv2.waitKey(0)
+cv2.waitKey(0)
